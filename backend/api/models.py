50b019e990209be8f26ce2f932906cc88d83c33f
Nour Bayar <nourbayar4@gmail.com>
2025-03-29 03:16:55 +0100

    back_covoiturage

--- backend/api/models.py
+++ backend/api/models.py
@@ -9,4 +9,32 @@ class User(models.Model):
 
     def __str__(self):
         return self.clerk_user_id
+    
+
+    
+class Voiture(models.Model):
+    id_voiture = models.AutoField(primary_key=True)
+    marque = models.CharField(max_length=255)
+    matricule = models.CharField(max_length=255, unique=True)
+    image = models.ImageField(upload_to='voitures/', null=True, blank=True)  # L'option pour stocker l'image de la voiture
+
+    def __str__(self):
+        return f"{self.marque} - {self.matricule}"
+
+class Trajet(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    phonenumber = models.CharField(max_length=8)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    departure = models.CharField(max_length=255)
+    arrival = models.CharField(max_length=255)
+    departure_date = models.DateTimeField()  # Date et heure du départ
+    arrival_date = models.DateTimeField()  # Date et heure d'arrivée
+    nb_places = models.IntegerField()  # Nombre de places disponibles
+    voiture = models.ForeignKey(Voiture, on_delete=models.CASCADE)  # Clé étrangère vers la table Voiture
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.id} - {self.name}: {self.departure} -> {self.arrival} ({self.departure_date})"
+
 # Create your models here.
